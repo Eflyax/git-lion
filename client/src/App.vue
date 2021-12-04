3fd1081825881ded8cfe38fe23bbdfb6fbbe0591
Jakub Záruba <jakub.zaruba@oorgis.cz>
2021-12-04 23:15:53 +0100

    WIP - UI

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,17 +1,44 @@
 <template>
 	<v-app>
+		<v-app-bar density="compact">
+			<v-app-bar-title>GitLion</v-app-bar-title>
+
+			<v-btn color="secondary">
+				vuepress
+			</v-btn>
+
+			<v-btn class="mr-2 ml-2" color="secondary">
+				the-game
+			</v-btn>
+
+			<v-spacer></v-spacer>
+
+			<v-divider inset vertical></v-divider>
+			<v-btn>
+				<v-icon
+					x-large
+					color="secondary"
+				>
+					mdi-cog
+				</v-icon>
+			</v-btn>
+		</v-app-bar>
+
 		<v-main>
+			<toolbar />
 			<router-view />
 		</v-main>
 	</v-app>
 </template>
 
 <script>
+import toolbar from './components/Toolbar.vue';
 
 export default {
 	name: 'App',
 
 	components: {
+		toolbar
 	},
 
 	data: () => ({
--- client/src/components/Home.vue
+++ client/src/components/Home.vue
@@ -1,14 +1,42 @@
 <template>
-	<div class="home">
-		<h1>Home</h1>
+	<div class="body">
+		<div class="left-panel"></div>
+		<div class="graph"></div>
+		<div class="sidebar"></div>
 	</div>
 </template>
 
-<script>
+<script lang="ts">
 export const Home = {
-	name: "Home"
-
+	name: "Home",
+	data() {
+		return {
+		};
+	}
 };
 
 export default Home;
 </script>
+<style>
+.body {
+	border: 1px solid #272830;
+	height: 100%;
+	display: flex;
+	flex-direction: row;
+}
+
+.left-panel {
+	flex: 1 0 0;
+	border: 2px solid #272830;
+}
+
+.graph {
+	flex: 5 0 0;
+	border: 2px solid #272830;
+}
+
+.sidebar {
+	flex: 2 0 0;
+	border: 2px solid #272830;
+}
+</style>
